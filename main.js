92afaa5ca53b439644858504e652a929d1233408
josetv91 <61167951+josetv91@users.noreply.github.com>
2021-01-04 02:39:11 -0500

    Update main.js

---
+++ main.js
@@ -0,0 +1,28 @@
+let out = document.getElementById('stopwatch');
+
+let mins = 0;
+let ms= 0;
+let sec = 0;
+
+function timer() {
+    ms++;
+    if(ms >= 100){
+        sec++
+        ms = 0
+    }
+    if(sec === 60){
+        min++
+        sec = 0
+    }
+    if(min === 60){
+        ms, sec, mins = 0;
+    }
+
+    //Doing some string interpolation
+    let millis = ms < 10 ? `0`+ ms : ms;
+    let seconds = sec < 10 ? `0`+ sec : sec;
+    let minutes = min < 10 ? `0` + min : min;
+
+    let timer= `${minutes}:${seconds}:${millis}`;
+    output.innerHTML =timer;
+};
