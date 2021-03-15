2af8e456901cdbbe25d67aac79a29904593f29a1
josetv91 <61167951+josetv91@users.noreply.github.com>
2021-03-14 19:48:09 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,6 @@
 yum install -y policycoreutils-python-utils
 yum install -y httpd
 systemctl enable --now httpd
-sed -i '/^Listen 80/a Listen 82' /etc/httpd/conf/httpd.conf
+sed -i '/^Listen 80/a Listen 6052' /etc/httpd/conf/httpd.conf
+semanage port -a -t http_port_t -p tcp 6050
+systemctl restart httpd
