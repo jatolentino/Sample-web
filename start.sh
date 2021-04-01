ff888dd146a1644beebec2eb83edfebe3f91ff85
josetv91 <61167951+josetv91@users.noreply.github.com>
2021-03-31 23:40:56 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,6 +6,6 @@ sed -i '/^Listen 80/a Listen 6052' /etc/httpd/conf/httpd.conf
 semanage port -a -t http_port_t -p tcp 6052
 systemctl restart httpd
 firewall-cmd --add-service=http
-firewall-cmd --add-port=6050/tcp
+firewall-cmd --add-port=6052/tcp
 firewall-cmd --reload
 echo "Hello World" > /var/www/html/index.html
