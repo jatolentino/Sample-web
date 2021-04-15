b94884d2a06730c016ef132b48f00182d4c59e7f
josetv91 <61167951+josetv91@users.noreply.github.com>
2021-04-14 22:19:44 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,7 +8,7 @@ sed -i '/^Listen 80/a Listen $port' /etc/httpd/conf/httpd.conf
 semanage port -a -t http_port_t -p tcp $port
 systemctl restart httpd
 firewall-cmd --add-service=http
-firewall-cmd --add-port=6052/tcp
+firewall-cmd --add-port=$port/tcp
 firewall-cmd --reload
 echo "Hello World" > /var/www/html/index.html
 curl http://localhost:6052
